c0d28e89f772869209bbe1411134c29265543177
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2021-06-03 10:15:19 -0300

    delete: remove projeto para replace atualizado

--- src/utils/PureFunctions.ts
+++
@@ -1,5 +0,0 @@
-function addingNumbers(a: number, b: number) {
-    return a + b;
-};
-
-export default addingNumbers;
--- src/utils/constants.ts
+++
@@ -1,4 +0,0 @@
-export enum Colors {
-    LIGHT_GREY = '#48485b',
-    DARKER_GREY = '#2c2c35',
-};
